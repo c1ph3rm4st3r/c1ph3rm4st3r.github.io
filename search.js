c8daafdee515a699783b26bb2cb8d45a1104f963
Dilanka Kaushal Hewage <66146701+k4u5h41@users.noreply.github.com>
2023-03-23 15:03:39 -0400

    Delete search.js

--- search.js
+++
@@ -1,7 +0,0 @@
-const index = new JekyllSearch({
-  jsonFile: '{{ site.baseurl }}/search.json',
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('search-results'),
-  limit: 10,
-  fuzzy: true
-});
